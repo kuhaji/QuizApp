778dd067d94036925faf1fa21558e7f965e6e02d
P0458 <p0458@student.jamk.fi>
2020-10-16 13:52:36 +0300

    Second commit

---
+++ app/src/main/java/com/example/quiz/Constants.kt
@@ -0,0 +1,109 @@
+package com.example.quiz
+
+
+
+object Constants {
+
+    // START
+    const val USER_NAME: String = "user_name"
+    const val TOTAL_QUESTIONS: String = "total_questions"
+    const val CORRECT_ANSWERS: String = "correct_answers"
+    // END
+
+    fun getQuestions(): ArrayList<Question> {
+        val questionsList = ArrayList<Question>()
+
+        // 1
+        val que1 = Question(
+            1, "How do you say this in finnish: What?",
+            "Mitä", "Kuka",
+            "Missä", "Milloin", 1
+        )
+
+        questionsList.add(que1)
+
+        // 2
+        val que2 = Question(
+            2, "How do you say this in finnish: Do you?",
+            "Minä", "Se",
+            "Sinä", "Hän", 3
+        )
+
+        questionsList.add(que2)
+
+        // 3
+        val que3 = Question(
+            3, "How do you say this in finnish: Like?",
+
+            "Vihata", "Epäillä",
+            "Inhota", "Tykätä", 4
+        )
+
+        questionsList.add(que3)
+
+        // 4
+        val que4 = Question(
+            4, "How do you say this in finnish: My?",
+            "Sinun", "Minun",
+            "Teidän", "Heidän", 2
+        )
+
+        questionsList.add(que4)
+
+        // 5
+        val que5 = Question(
+            5, "How do you say this in finnish: Application?",
+            "Tietokone", "Puhelin",
+            "Sovellus", "Internet", 3
+        )
+
+        questionsList.add(que5)
+
+        // 6
+        val que6 = Question(
+            6, "How do you say this in finnish: I hope?",
+            "Toivon", "Epäilen",
+            "Ihailen", "none of these", 1
+        )
+
+        questionsList.add(que6)
+
+        // 7
+        val que7 = Question(
+            7, "How do you say this in finnish: This?",
+            "Se", "Tuo",
+            "Tämä", "Ne", 3
+        )
+
+        questionsList.add(que7)
+
+        // 8
+        val que8 = Question(
+            8, "How do you say this in finnish: Assignment?",
+            "Kurssi", "Projekti",
+            "Työ", "Tehtävä", 4
+        )
+
+        questionsList.add(que8)
+
+        // 9
+        val que9 = Question(
+            9, "How do you say this in finnish: Will?",
+            "Vihata", "Haluta",
+            "Inhota", "Kävellä", 2
+        )
+
+        questionsList.add(que9)
+
+        // 10
+        val que10 = Question(
+            10, "How do you say this in finnish: Pass?",
+            "Läpi", "Hylätty",
+            "Ihan ok", "Tosi hyvä", 1
+        )
+
+        questionsList.add(que10)
+
+        return questionsList
+    }
+}
---
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.quiz
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+//main activity which collects players name and after entering name goes to quiz activity
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        btn_start.setOnClickListener {
+
+            if (et_name.text.toString().isEmpty()){
+                Toast.makeText(this,
+                    "Please enter your name", Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                intent.putExtra(Constants.USER_NAME, et_name.text.toString())
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+}
